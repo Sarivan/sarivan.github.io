1364d05d59b21484357081bef86939f43b4af90b
sariVan <sc08359h@hotmail.co.jp>
2020-01-13 00:12:12 +0900

    [update] play movie about fadein & fadeout

--- movie/movie2.js
+++ movie/movie2.js
@@ -1,14 +1,81 @@
-// PlayVideo
 var video;
+var isFadeout = false;
+var isFadein = false;
+var rectAlpha = 0;
+var fadeSpeed = 1;
+var isPaused = false;
+
+var videoWidth = 710;
+var videoHeight = 400;
+var now_frame;
 
 function setup() {
-  createCanvas(640, 320);
-  video = createVideo([ "movie.webm", "movie.mp4"]);
+  createCanvas(videoWidth, videoHeight);
+  video = createVideo(['assets/fingers.mov', 'assets/fingers.webm']);
   video.hide();
   video.loop();
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
-   filter(INVERT);
+  background(150);
+  if(!isPaused)
+  {
+    now_frame = video;
+    image(now_frame, 0, 0);
+  }
+  else
+  {
+    image(now_frame, 0, 0);
+  }
+    noStroke();
+    fill(0, 0, 0, rectAlpha);
+    rect(0, 0, videoWidth, videoHeight);
+
+    if(isFadeout)
+    {
+      fadeout();
+    }
+  
+  if(isFadein)
+  {
+    fadein();
+  }
+}
+
+function fadeout()
+{
+  rectAlpha += fadeSpeed;
+  if(rectAlpha > 255)
+  {
+    rectAlpha = 255;
+    isFadeout = false;
+    isPaused = true;
+  }
+}
+
+function fadein()
+{
+  rectAlpha -= fadeSpeed; 
+  if(rectAlpha < 0)
+  {
+    rectAlpha = 0;
+    isFadein = false;
+  }
+}
+
+function keyPressed()
+{
+  if(!isFadeout)
+  {
+    isFadeout = true;
+  }
+  else if(!isFadein && isPaused)
+  {
+    isPaused = false;
+    isFadein = true;
+  }
+}
+
+function mousePressed()
+{
 }
